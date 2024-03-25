739477db03dbd46ffaf406cd6064f4262348a7c5
filiprendon <126860088+filiprendon@users.noreply.github.com>
2024-03-25 17:06:50 +0100

    btn back to top

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-// crear boton back to top
+const backTT = document.createElement('button');
+createBtn();
+
+// Crear botón back to top
+function createBtn() {
+    backTT.innerHTML = '&#xF148';
+    backTT.style.position = 'fixed';
+    backTT.style.bottom = '30px';
+    backTT.style.right = '30px';
+    backTT.style.borderRadius = '25px';
+    backTT.style.width = '50px';
+    backTT.style.height = '50px';
+    backTT.style.backgroundColor = '#ffd16f';
+    backTT.style.color = 'white';
+    backTT.style.border = 'none';
+    backTT.style.cursor = 'pointer';
+    backTT.id = "back-to-top";
+    backTT.addEventListener('click', scrollToTop);
+    document.body.appendChild(backTT);
+}
+
+function backToTop() {
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+        backTT.style.display = 'block';
+    } else {
+        backTT.style.display = 'none';
+    }
+}
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+window.addEventListener('scroll', backToTop);
