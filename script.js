240357250426509fd9f546ab609083aac4134df9
filiprendon <126860088+filiprendon@users.noreply.github.com>
2024-03-28 19:38:28 +0100

    Color h1 cambia scroll

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const backtt = document.getElementById('backtt');
 createBtn();
+// Borra la url
+// window.history.replaceState('','','/');
 
 function createBtn() {
     // backtt.innerHTML = '&#xF148';
@@ -14,17 +16,20 @@ function createBtn() {
     backtt.style.border = 'none';
     backtt.style.cursor = 'pointer';
     backtt.id = "back-to-top";
+    // Pongo display none para ocultarlo al recargar página
+    backtt.style.display = 'none';
     backtt.addEventListener('click', scrollToTop);
     document.body.appendChild(backtt);
 }
 
 function backToTop() {
+
     if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
         backtt.style.display = 'block';
     } else {
         backtt.style.display = 'none';
     }
-    // Comprobar k el contenedor esté arriba
+    // Comprobar que el contenedor esté arriba
     const containers = document.querySelectorAll('.container-fluid');
     containers.forEach(container => {
         const rect = container.getBoundingClientRect();
@@ -44,3 +49,34 @@ function scrollToTop() {
 }
 
 window.addEventListener('scroll', backToTop);
+
+// Cambiar colores h1 a medida k el usuario haga scroll
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener('scroll', function () {
+        let mainContainer = document.getElementById('main').getElementsByTagName('h1')[0];
+        let diffConteiner = document.getElementsByClassName('diff-container');
+
+        // Se obtiene la posición del h1 del main
+        var mainContainerPosition = mainContainer.getBoundingClientRect().top;
+
+        // Verificar si están en la misma posición vertical
+        for (var i = 0; i < diffConteiner.length; i++) {
+            var diffContainerPosition = diffConteiner[i].getBoundingClientRect().top;
+
+            // Si el h1 de otro contenedor está cerca de la posición del h1 principal, cambio su color gradualmente
+            if (diffContainerPosition <= mainContainerPosition) {
+                if (diffConteiner[i].classList.contains('pr')) {
+                    diffConteiner[i].style.transition = "color 0s ease";
+                    diffConteiner[i].style.color = "#d3d3d3";
+                }
+                else if (diffConteiner[i].classList.contains('im')) {
+                    diffConteiner[i].style.transition = "color 0s ease";
+                    diffConteiner[i].style.color = "#464646";
+                }
+
+            } else {
+                diffConteiner[i].style.color = "transparent";
+            }
+        }
+    });
+});
